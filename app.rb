9157a66dd05fee4f92cc3661bb0acaa7735b1ff6
Kat Gifford and Sean Pierce <sean@>
2017-04-19 16:07:02 -0700

    add logic to winner page

--- app.rb
+++ app.rb
@@ -10,15 +10,5 @@ get('/') do
 end
 
 get('/winner') do
-  player_turn = Turn.new
-  comp_turn = Turn.new
-  @player = player_turn.rando
-  @comp = comp_turn.rando
-  if (@player[0].beats?(@comp[0]))
-    erb(:win1)
-  elsif (@comp[0].beats?(@player[0]))
-    erb(:win2)
-  else
-    erb(:winner)
-  end
+  erb(:winner)
 end
---
+++ spec/rps_integration.rb
@@ -0,0 +1,11 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+
+describe('the path to game outcome', {:type => :feature}) do
+  it('processes the user turn and computer turn and returns game results') do
+    visit('/')
+    click_btn('Player 1 play!')
+    expect(page).to have_content
+  end
+end

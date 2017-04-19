b86eb65ab3bb26422c2edea97df0c706ee9bac33
Kat Gifford and Sean Pierce <kat@>
2017-04-19 15:28:25 -0700

    successfully display both player turn and computer turn to winner page

--- app.rb
+++ app.rb
@@ -8,3 +8,11 @@ require('pry')
 get('/') do
   erb(:index)
 end
+
+get('/winner') do
+  player_turn = Turn.new
+  comp_turn = Turn.new
+  @player = player_turn.rando
+  @comp = comp_turn.rando
+  erb(:winner)
+end

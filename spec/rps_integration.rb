8564c18a56685f84e3097d60cca099e59e0673c8
Kat Gifford and Sean Pierce <kat@>
2017-04-19 16:26:09 -0700

    add custom button that routes to winner page.

--- spec/rps_integration.rb
+++ spec/rps_integration.rb
@@ -5,7 +5,7 @@ Capybara.app = Sinatra::Application
 describe('the path to game outcome', {:type => :feature}) do
   it('processes the user turn and computer turn and returns game results') do
     visit('/')
-    click_btn('Player 1 play!')
+    click_button('Player 1 play!')
     expect(page).to have_content
   end
 end

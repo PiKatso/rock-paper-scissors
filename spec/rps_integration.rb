8016eeffd3c081f62573ab0a7b0e510c434f88d9
Kat Gifford and Sean Pierce <kat@>
2017-04-19 16:39:27 -0700

    refactor

--- spec/rps_integration.rb
+++ spec/rps_integration.rb
@@ -6,6 +6,6 @@ describe('the path to game outcome', {:type => :feature}) do
   it('processes the user turn and computer turn and returns game results') do
     visit('/')
     click_button('Player 1 play!')
-    expect(page).to have_content
+    expect(page).to have_content(@player)
   end
 end
